bb9325a2dc64ae114512557ba542913655b58cb5
Stian Didriksen <stipsan@gmail.com>
2016-06-09 23:21:27 +0200

    adding incr test

--- test/incr.js
+++ test/incr.js
@@ -4,6 +4,12 @@ import MockRedis from '../src';
 
 describe('incr', () => {
   it('should increment an integer', () => {
+    const redis = new MockRedis({
+      data: {
+        user_next: '1',
+      },
+    });
 
+    redis.incr('user_next').then(userNext => expect(userNext).toBe('2'));
   });
 });
