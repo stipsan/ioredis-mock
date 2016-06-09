1f79bb3101a31e1b7539ef883ca9db212588c8ce
Stian Didriksen <stipsan@gmail.com>
2016-06-09 09:42:02 +0200

    adding the mocker class
    still a lot of work to do, but this is what I got so far

---
+++ src/index.js
@@ -0,0 +1,92 @@
+import _ from 'lodash'
+
+class RedisMock {
+  constructor({ data }) {
+    this.data = data;
+  }
+  incr(key) {
+    return new Promise(resolve => {
+      const curVal = Number(this.data[key])
+      const incVal = curVal + 1
+      resolve(incVal.toString())
+    })
+  }
+  hsetnx(key, hashKey, hashVal) {
+    return new Promise(resolve => {
+
+      if (!this.data.hasOwnProperty(key)) {
+        this.data[key] = {}
+      }
+      const exists = this.data[key].hasOwnProperty(hashKey)
+      this.data[key][hashKey] = hashVal
+
+      resolve(!exists)
+    })
+  }
+  hmset(key, ...hmsetData) {
+    return new Promise(resolve => {
+      if (!this.data.hasOwnProperty(key)) {
+        this.data[key] = {}
+      }
+      for (let i = 0; i < hmsetData.length; i += 2) {
+        this.data[key][hmsetData[i]] = hmsetData[i + 1]
+      }
+
+      resolve('OK')
+    })
+  }
+  sadd(key, val) {
+    return new Promise(resolve => {
+      this.data[key].push(val)
+      resolve(1)
+    })
+  }
+  srem(key, val) {
+    return new Promise(resolve => {
+      const index = this.data[key].indexOf(val)
+      this.data[key].splice(index, 1)
+      resolve(1)
+    })
+  }
+  hget(key, value) {
+    return new Promise(resolve => {
+      return resolve(this.data[key][value])
+    })
+  }
+  hvals(key) {
+    return new Promise(resolve => {
+      resolve(_.values(this.data[key]))
+    })
+  }
+  hgetall(key) {
+    return new Promise((resolve, reject) => {
+      resolve(this.data[key])
+    })
+  }
+  smembers(key) {
+    return new Promise((resolve, reject) => {
+      resolve(this.data[key])
+    })
+  }
+  sismember(key, val) {
+    return new Promise(resolve => {
+      resolve(this.data[key].indexOf(val) !== -1)
+    })
+  }
+  hset() {
+    return new Promise(resolve => {
+      resolve('OK')
+    })
+  }
+  multi(batch) {
+    this.batch = batch.map(([command, ...options]) => this[command].bind(this, ...options))
+
+    return this
+  }
+  exec() {
+    return Promise.all(this.batch.map(promise => promise()))
+      .then(results => results.map(result => [null, result]))
+  }
+}
+
+export default RedisMock
