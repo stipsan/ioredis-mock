8cdb53015e1a96ed252b6c570e4eead6a9191ec6
Stian Didriksen <stipsan@me.com>
2016-06-10 11:19:09 +0200

    trying again, coveralls should throw an error

--- test/hset.js
+++ test/hset.js
@@ -1,14 +1,4 @@
-import expect from 'expect';
-
-import MockRedis from '../src';
 
 describe('hset', () => {
-  const redis = new MockRedis({
-    data: {
-      emails: {},
-    },
-  });
-  it('should return OK when setting a hash key', () =>
-    redis.hset('emails', 'clark@daily.planet', '1').then(status => expect(status).toBe('OK'))
-  );
+  it('should return OK when setting a hash key');
 });
