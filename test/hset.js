d7b823d39b246551217dff93fb170751aed2c086
Stian Didriksen <stipsan@me.com>
2016-06-10 11:12:07 +0200

    deleting a test to reduce test coverage
    this is intentional, in order to test the coveralls integration

--- test/hset.js
+++ test/hset.js
@@ -11,7 +11,4 @@ describe('hset', () => {
   it('should return OK when setting a hash key', () =>
     redis.hset('emails', 'clark@daily.planet', '1').then(status => expect(status).toBe('OK'))
   );
-  it('should be returning the updated data in hget', () =>
-    redis.hget('emails', 'clark@daily.planet').then(userId => expect(userId).toBe('1'))
-  );
 });
