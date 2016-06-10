631147d6b43fec6ed458b637a64bbffc994743ed
Stian Didriksen <stipsan@gmail.com>
2016-06-10 13:18:23 +0200

    simpler test, now with 100% coverage

--- test/hsetnx.js
+++ test/hsetnx.js
@@ -3,20 +3,14 @@ import expect from 'expect';
 import MockRedis from '../src';
 
 describe('hsetnx', () => {
-  const redis = new MockRedis({
-    data: {
-      emails: {
-        'clark@daily.planet': '1',
-      },
-    },
-  });
+  const redis = new MockRedis();
   it('should set a key in a hash map if it does not exist already', () =>
-    redis.hsetnx('emails', 'bruce@wayne.enterprises', '2')
+    redis.hsetnx('emails', 'bruce@wayne.enterprises', '1')
          .then(userNext => expect(userNext).toBeTruthy())
          .then(() => {
            expect(redis.data.emails['bruce@wayne.enterprises'])
-           .toBe('2', 'hash map value persisted');
-           return redis.hsetnx('emails', 'clark@daily.planet', '2');
+           .toBe('1', 'hash map value persisted');
+           return redis.hsetnx('emails', 'bruce@wayne.enterprises', '2');
          })
          .then(userNext => expect(userNext).toBeFalsy('hsetnx no-op failed on existing key'))
   );
