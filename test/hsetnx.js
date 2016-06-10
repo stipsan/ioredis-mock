7f297fcb976451acdb3455de6a99a2fe31e3db33
Stian Didriksen <stipsan@gmail.com>
2016-06-10 13:09:25 +0200

    test coverage on hsetnx should be 100%
    also fixing duplicate code issue reported by codeclimate

--- test/hsetnx.js
+++ test/hsetnx.js
@@ -10,12 +10,13 @@ describe('hsetnx', () => {
       },
     },
   });
-  it('should set a key in a hashmap', () =>
+  it('should set a key in a hash map if it does not exist already', () =>
     redis.hsetnx('emails', 'bruce@wayne.enterprises', '2')
          .then(userNext => expect(userNext).toBeTruthy())
-  );
-  it('should no-op if key already exists', () =>
-    redis.hsetnx('emails', 'clark@daily.planet', '2')
-         .then(userNext => expect(userNext).toBeFalsy())
+         .then(() => {
+           expect(redis.data.emails['bruce@wayne.enterprises']).toBe('2', 'hash map value persisted');
+           return redis.hsetnx('emails', 'clark@daily.planet', '2');
+         })
+         .then(userNext => expect(userNext).toBeFalsy('hsetnx no-op failed on existing key'))
   );
 });
