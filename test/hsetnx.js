a2a0cba8947f335955a64348c192bda41b2c964e
Stian Didriksen <stipsan@gmail.com>
2016-06-12 13:51:41 +0200

    added hmset test

--- test/hsetnx.js
+++ test/hsetnx.js
@@ -3,15 +3,15 @@ import expect from 'expect';
 import MockRedis from '../src';
 
 describe('hsetnx', () => {
-  const redis = new MockRedis();
-  it('should set a key in a hash map if it does not exist already', () =>
-    redis.hsetnx('emails', 'bruce@wayne.enterprises', '1')
-         .then(userNext => expect(userNext).toBeTruthy())
+  it('should set a key in a hash map if it does not exist already', () => {
+    const redis = new MockRedis();
+    return redis.hsetnx('emails', 'bruce@wayne.enterprises', '1')
+         .then(userNext => expect(userNext).toBe(true))
          .then(() => {
            expect(redis.data.emails['bruce@wayne.enterprises'])
            .toBe('1', 'hash map value failed to persist');
            return redis.hsetnx('emails', 'bruce@wayne.enterprises', '2');
          })
-         .then(userNext => expect(userNext).toBeFalsy('hsetnx no-op failed on existing key'))
-  );
+         .then(userNext => expect(userNext).toBeFalsy('hsetnx no-op failed on existing key'));
+  });
 });
