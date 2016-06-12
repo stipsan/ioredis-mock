d36276f526b9cd978c253cec9d7998eaf1c264a9
Stian Didriksen <stipsan@gmail.com>
2016-06-12 14:31:20 +0200

    sadd and srem now allows multiple items

--- src/index.js
+++ src/index.js
@@ -34,20 +34,22 @@ class RedisMock {
       resolve('OK');
     });
   }
-  sadd(key, val) {
+  sadd(key, ...vals) {
     return new Promise(resolve => {
       if (!this.data.hasOwnProperty(key)) {
         this.data[key] = [];
       }
-      this.data[key].push(val);
-      resolve(1);
+      this.data[key].push(...vals);
+      resolve(vals.length);
     });
   }
-  srem(key, val) {
+  srem(key, ...vals) {
     return new Promise(resolve => {
-      const index = this.data[key].indexOf(val);
-      this.data[key].splice(index, 1);
-      resolve(1);
+      vals.forEach(val => {
+        const index = this.data[key].indexOf(val);
+        this.data[key].splice(index, 1);
+      });
+      resolve(vals.length);
     });
   }
   hget(key, hashKey) {
