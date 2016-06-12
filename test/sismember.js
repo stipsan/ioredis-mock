0dc46d2703c72abfaa1d37b3270703753838c564
Stian Didriksen <stipsan@gmail.com>
2016-06-12 14:18:12 +0200

    adding sismember test

--- test/sismember.js
+++ test/sismember.js
@@ -1,3 +1,17 @@
+import expect from 'expect';
+
+import MockRedis from '../src';
+
 describe('sismember', () => {
-  it('should check if item exists in list');
+  it('should check if item exists in list', () => {
+    const redis = new MockRedis({
+      data: {
+        foos: ['foo', 'bar'],
+      },
+    });
+
+    return redis.sismember('foos', 'foo').then(result => expect(result).toBe(true))
+                .then(() => redis.sismember('foos', 'foobar'))
+                .then(result => expect(result).toBe(false));
+  });
 });
