99dc5657352c79135ae46741ca0cff121bdc47ff
Stian Didriksen <stipsan@gmail.com>
2016-06-12 14:23:08 +0200

    adding multi test

--- test/multi.js
+++ test/multi.js
@@ -1,3 +1,18 @@
+import expect from 'expect';
+
+import MockRedis from '../src';
+
 describe('multi', () => {
-  it('should setup a batch queue that can be passed to exec');
+  it('should setup a batch queue that can be passed to exec', () => {
+    const redis = new MockRedis();
+
+    redis.multi([
+      ['incr', 'user_next'],
+      ['incr', 'post_next'],
+    ]);
+    expect(redis.batch).toBeA('array');
+    expect(redis.batch.length).toBe(2);
+    expect(redis.batch[0]).toBeA('function');
+    expect(redis.batch[1]).toBeA('function');
+  });
 });
