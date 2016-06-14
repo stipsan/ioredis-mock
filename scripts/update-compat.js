4e9690e9ac50ef2585d007efd50ab3a9a7b9fdcd
Stian Didriksen <stipsan@gmail.com>
2016-06-14 19:25:46 +0200

    fix linting

--- scripts/update-compat.js
+++ scripts/update-compat.js
@@ -1,22 +1,21 @@
-var path = require('path');
-var fs = require('fs');
-var commands = require('redis-commands');
-var redis = require('ioredis');
-var redisMock = require('..').default;
+const path = require('path');
+const fs = require('fs');
+const commands = require('redis-commands');
+const redis = require('ioredis');
+const redisMock = require('..').default;
 
-var tableMd = `
+let tableMd = `
 ### Supported commands
 | redis | ioredis | ioredis-mock |
 |-------|:-------:|:------------:|`;
 commands.list.forEach(command => {
-  var redisCol = `${command}`;
-  var ioredisCol = command in redis.prototype ? ':white_check_mark:' : ':x:';
-  var ioredisMockCol = command in redisMock.prototype ? ':white_check_mark:' : ':x:';
+  const redisCol = `${command}`;
+  const ioredisCol = command in redis.prototype ? ':white_check_mark:' : ':x:';
+  const ioredisMockCol = command in redisMock.prototype ? ':white_check_mark:' : ':x:';
   tableMd += `
 |${redisCol}|${ioredisCol}|${ioredisMockCol}|`;
 });
 
 fs.writeFile(path.resolve('..', 'compat.md'), tableMd, (err) => {
   if (err) throw err;
-  console.log('Compat table generated!');
 });
