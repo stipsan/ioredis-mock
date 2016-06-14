b6d620b82e2854eb3686b5f70f8d6357c1ca7717
Stian Didriksen <stipsan@gmail.com>
2016-06-14 20:29:24 +0200

    working on a reusable command wrapper

---
+++ test/command.js
@@ -0,0 +1,11 @@
+describe('basic command', () => {
+  it('should return a Promise that resolves the returned value', () => {
+
+  });
+
+  it('should support node style callbacks', () => {
+
+  });
+
+  it('should reject the promise if the first argument is bool false to allow simulating failures');
+});
