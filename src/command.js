1fe0d988a16533fd546049e7997d1e121993c587
Stian Didriksen <stipsan@gmail.com>
2016-06-14 21:05:58 +0200

    bluebird implemented
    and node style callback supported

--- src/command.js
+++ src/command.js
@@ -1,3 +1,20 @@
+import Promise from 'bluebird';
+
 export default function command(emulate) {
-  return (...args) => new Promise(resolve => resolve(emulate(...args)));
+  return (...args) => {
+    let length = args.length;
+    const lastArgIndex = length - 1;
+    let callback = args[lastArgIndex];
+    if (typeof callback !== 'function') {
+      callback = undefined;
+    } else {
+      length = lastArgIndex;
+    }
+    const newArgs = new Array(length);
+    for (let i = 0; i < length; ++i) {
+      newArgs[i] = args[i];
+    }
+
+    return new Promise(resolve => resolve(emulate(...newArgs))).asCallback(callback);
+  };
 }
