ececf9900a11c68cdec98e7c5e9a2f8292a08425
Stian Didriksen <stipsan@gmail.com>
2016-06-21 00:26:32 +0200

    fix broken test

--- test/commands/time.js
+++ test/commands/time.js
@@ -5,11 +5,11 @@ import MockRedis from '../../src';
 describe('time', () => {
   it('should return an array with current time in seconds and microseconds', () => {
     const redis = new MockRedis();
-    const time = microtime.nowStruct();
+    const time = Math.round(new Date().getTime() / 1000);
 
     return redis.time().then(result => {
-      expect(result[0]).toBeGreaterThanOrEqualTo(time[0]);
-      expect(result[0]).toBeLessThan(time[0] + 10);
+      expect(result[0]).toBeGreaterThanOrEqualTo(time);
+      expect(result[0]).toBeLessThan(time + 10);
       expect(result[1]).toBeA('number');
     });
   });
