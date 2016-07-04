aac274e06ab7311530cb0bd6b99f458e031eba0e
Stian Didriksen <stipsan@gmail.com>
2016-07-04 15:42:50 +0200

    hsetnx lied when it said it didn't overwrite

--- test/commands/hsetnx.js
+++ test/commands/hsetnx.js
@@ -6,12 +6,16 @@ describe('hsetnx', () => {
   it('should set a key in a hash map if it does not exist already', () => {
     const redis = new MockRedis();
     return redis.hsetnx('emails', 'bruce@wayne.enterprises', '1')
-         .then(userNext => expect(userNext).toBe(true))
+         .then(status => expect(status).toBe('1'))
          .then(() => {
            expect(redis.data.emails['bruce@wayne.enterprises'])
            .toBe('1', 'hash map value failed to persist');
            return redis.hsetnx('emails', 'bruce@wayne.enterprises', '2');
          })
-         .then(userNext => expect(userNext).toBeFalsy('hsetnx no-op failed on existing key'));
+         .then(status => expect(status).toBe('0', 'hsetnx no-op failed on existing key'))
+         .then(() => {
+           expect(redis.data.emails['bruce@wayne.enterprises'])
+           .toBe('1', 'existing hash map value was overwritten');
+         });
   });
 });
