5875a12ed36f0e2dad222cb0b45a17c66d3dc941
David Corwin <dave@launchpadlab.com>
2016-08-11 17:54:36 -0500

    add support for objects in hmset

--- src/commands/incr.js
+++ src/commands/incr.js
@@ -1,4 +1,7 @@
 export function incr(key) {
+  if (!this.data[key]) {
+    this.data[key] = '0';
+  }
   const curVal = Number(this.data[key]);
   this.data[key] = curVal + 1;
   return this.data[key].toString();
