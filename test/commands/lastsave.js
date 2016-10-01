ac602ca44fa1c2d2aeb148bbb549e16c1ec004af
Stian Didriksen <stipsan@gmail.com>
2016-10-01 22:01:48 +0200

    add lastsave command

--- test/commands/lastsave.js
+++ test/commands/lastsave.js
@@ -6,7 +6,8 @@ describe('lastsave', () => {
   it('should return unix time since last save', () => {
     const redis = new MockRedis();
 
-    return redis.lastsave()
-    .then(result => expect(result).toBeLessThanOrEqualTo(Math.floor(new Date().getTime() / 1000)));
+    return redis.lastsave().then(result =>
+      expect(result).toBeLessThanOrEqualTo(Math.floor(new Date().getTime() / 1000))
+    );
   });
 });
