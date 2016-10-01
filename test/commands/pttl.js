7c723b478a65c5f2fa8730ea679e76c564cd56af
Stian Didriksen <stipsan@gmail.com>
2016-10-02 00:46:07 +0200

    added `pttl` command

---
+++ test/commands/pttl.js
@@ -0,0 +1,32 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('pttl', () => {
+  it('should return -2 if key does not exist', () => {
+    const redis = new MockRedis();
+
+    return redis.pttl('foo').then(result => expect(result).toBe('-2'));
+  });
+
+  it('should return -1 if key exist but have no expire', () => {
+    const redis = new MockRedis({
+      data: {
+        foo: 'bar',
+      },
+    });
+
+    return redis.pttl('foo').then(result => expect(result).toBe('-1'));
+  });
+
+  it('should return seconds left until expire', () => {
+    const redis = new MockRedis({
+      data: {
+        foo: 'bar',
+      },
+    });
+
+    return redis.expire('foo', 1).then(() => redis.pttl('foo'))
+      .then(result => expect(result).toBe('1000'));
+  });
+});
