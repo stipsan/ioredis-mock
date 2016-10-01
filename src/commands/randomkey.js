f5f4ed85bcee5a0c08132f856d9a0940359be250
Stian Didriksen <stipsan@gmail.com>
2016-10-01 23:54:23 +0200

    fix logic error

--- src/commands/randomkey.js
+++ src/commands/randomkey.js
@@ -2,5 +2,5 @@ import { random } from 'lodash';
 
 export function randomkey() {
   const keys = this.data.keys();
-  return keys.length > 0 ? keys[random(0, keys.length)] : null;
+  return keys.length > 0 ? keys[random(0, keys.length - 1)] : null;
 }
