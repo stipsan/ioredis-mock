80a02857b6cc5ff6b4fab7d35a7e7d00f5c8c9bb
Stian Didriksen <stipsan@gmail.com>
2016-10-01 23:50:14 +0200

    add randomkey command

---
+++ src/commands/randomkey.js
@@ -0,0 +1,6 @@
+import { random } from 'lodash';
+
+export function randomkey() {
+  const keys = this.data.keys();
+  return keys.length > 0 ? keys[random(0, keys.length)] : null;
+}
---
+++ test/commands/randomkey.js
@@ -0,0 +1,23 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('randomkey', () => {
+  it('should return a random key', () => {
+    const redis = new MockRedis({
+      data: {
+        foo: 'bar',
+        bar: 'foo',
+      },
+    });
+
+    return redis.randomkey()
+      .then(result => expect(['foo', 'bar']).toInclude(result));
+  });
+
+  it('should return null if db is empty', () => {
+    const redis = new MockRedis();
+
+    return redis.randomkey().then(result => expect(result).toBe(null));
+  });
+});
