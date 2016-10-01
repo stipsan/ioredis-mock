f3939e468b2df1fc00038a74ca5ef777831da6b9
Stian Didriksen <stipsan@me.com>
2016-10-02 01:14:04 +0200

    added `pexpireat` command (#176)

---
+++ src/commands/pexpireat.js
@@ -0,0 +1,9 @@
+export function pexpireat(key, at) {
+  if (!this.data.has(key)) {
+    return '0';
+  }
+
+  this.expires.set(key, at);
+
+  return '1';
+}
---
+++ test/commands/pexpireat.js
@@ -0,0 +1,26 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('pexpireat', () => {
+  it('should set expire status on key', () => {
+    const redis = new MockRedis({
+      data: {
+        foo: 'bar',
+      },
+    });
+    const at = Date.now() + 2000;
+    return redis.pexpireat('foo', at).then((status) => {
+      expect(status).toBe('1');
+      expect(redis.expires.has('foo')).toBe(true);
+
+      return redis.ttl('foo');
+    }).then(result => expect(parseInt(result, 10)).toBeGreaterThanOrEqualTo(1));
+  });
+
+  it('should return 0 if key does not exist', () => {
+    const redis = new MockRedis();
+    const at = Date.now();
+    return redis.pexpireat('foo', at).then(status => expect(status).toBe('0'));
+  });
+});
