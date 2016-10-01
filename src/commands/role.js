48317780e439d21043d5ef6efd269a2a18291b9b
Stian Didriksen <stipsan@me.com>
2016-10-02 00:05:37 +0200

    add role command (#168)

---
+++ src/commands/role.js
@@ -0,0 +1,3 @@
+export function role() {
+  return ['master', 0];
+}
---
+++ test/commands/role.js
@@ -0,0 +1,13 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('role', () => {
+  it('should return role info on the current redis instance', () => {
+    const redis = new MockRedis();
+
+    return redis.role().then(result => expect(result).toEqual(['master', 0]));
+  });
+
+  it('should return slave info');
+});
