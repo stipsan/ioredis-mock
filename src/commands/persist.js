013c1c6f2035289f06be35979801701c5ff095ef
Stian Didriksen <stipsan@me.com>
2016-10-02 00:33:26 +0200

    add persist command (#171)
    * add persist command
    
    * fix export

---
+++ src/commands/persist.js
@@ -0,0 +1,13 @@
+export function persist(key) {
+  if (!this.data.has(key)) {
+    return '0';
+  }
+
+  if (!this.expires.has(key)) {
+    return '0';
+  }
+
+  this.expires.delete(key);
+
+  return '1';
+}
