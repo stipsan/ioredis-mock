02420319919bffe2c1790c776a7b5df01d4c063d
Stian Didriksen <stipsan@gmail.com>
2016-10-01 23:13:57 +0200

    add echo command

---
+++ test/commands/echo.js
@@ -0,0 +1,11 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('echo', () => {
+  it('should return message', () => {
+    const redis = new MockRedis();
+
+    return redis.echo('Hello World!').then(result => expect(result).toBe('Hello World!'));
+  });
+});
