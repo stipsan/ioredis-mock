1189a7fb99006db8e1015f827630d8211c2996d2
Stian Didriksen <stipsan@gmail.com>
2016-10-01 21:40:35 +0200

    add auth command

---
+++ test/commands/auth.js
@@ -0,0 +1,11 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('auth', () => {
+  it('should return OK', () => {
+    const redis = new MockRedis();
+
+    return redis.auth('123456').then(status => expect(status).toBe('OK'));
+  });
+});
