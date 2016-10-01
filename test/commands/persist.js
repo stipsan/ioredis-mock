eb1dbc4b56c45e27ab98f0b289b6e0acc776ec99
Stian Didriksen <stipsan@gmail.com>
2016-10-02 00:28:00 +0200

    add persist command

---
+++ test/commands/persist.js
@@ -0,0 +1,34 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('persist', () => {
+  it('should remove expire status on key', () => {
+    const redis = new MockRedis({
+      data: {
+        foo: 'bar',
+      },
+    });
+    return redis.expire('foo', 1)
+      .then(() => redis.persist('foo'))
+      .then((status) => {
+        expect(status).toBe('1');
+        expect(redis.expires.has('foo')).toBe(false);
+      });
+  });
+
+  it('should return 0 if key does not have expire status', () => {
+    const redis = new MockRedis({
+      data: {
+        foo: 'bar',
+      },
+    });
+    return redis.persist('foo').then(status => expect(status).toBe('0'));
+  });
+
+  it('should return 0 if key does not exist', () => {
+    const redis = new MockRedis();
+
+    return redis.persist('foo').then(status => expect(status).toBe('0'));
+  });
+});
