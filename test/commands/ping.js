ef6c735c0088654a6267e17b3c1c0878368e2e35
Stian Didriksen <stipsan@me.com>
2016-10-02 00:21:26 +0200

    add ping command (#170)

---
+++ test/commands/ping.js
@@ -0,0 +1,17 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('ping', () => {
+  it('should return PONG', () => {
+    const redis = new MockRedis();
+
+    return redis.ping().then(result => expect(result).toBe('PONG'));
+  });
+
+  it('should return message', () => {
+    const redis = new MockRedis();
+
+    return redis.ping('Hello World!').then(result => expect(result).toBe('Hello World!'));
+  });
+});
