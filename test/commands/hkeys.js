eacb7fa3f76c4277b76679383c13e64706707085
Stian Didriksen <stipsan@gmail.com>
2016-10-02 00:55:23 +0200

    added `hkeys` command

---
+++ test/commands/hkeys.js
@@ -0,0 +1,21 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('hkeys', () => {
+  it('should return an empty array if there are no keys', () => {
+    const redis = new MockRedis();
+
+    return redis.hkeys('foo').then(result => expect(result).toEqual([]));
+  });
+
+  it('should return all data keys', () => {
+    const redis = new MockRedis({
+      data: {
+        foo: { bar: '1', baz: '2' },
+      },
+    });
+
+    return redis.hkeys('foo').then(result => expect(result).toEqual(['bar', 'baz']));
+  });
+});
