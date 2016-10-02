0828170feb7d0dfa2da2ced93a71e2c523a52d10
Stian Didriksen <stipsan@gmail.com>
2016-10-02 03:14:19 +0200

    added `hincrbyfloat` command

---
+++ test/commands/hincrbyfloat.js
@@ -0,0 +1,29 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('hincrbyfloat', () => {
+  it('should increment an float with passed increment', () => {
+    const redis = new MockRedis({
+      data: {
+        mykey: { field: '10.50' },
+      },
+    });
+
+    return redis.hincrbyfloat('mykey', 'field', 0.1)
+      .then(result => expect(result).toBe('10.6'))
+      .then(() => redis.hincrbyfloat('mykey', 'field', -5))
+      .then(result => expect(result).toBe('5.6'));
+  });
+
+  it('should support exponents', () => {
+    const redis = new MockRedis({
+      data: {
+        mykey: { field: '5.0e3' },
+      },
+    });
+
+    return redis.hincrbyfloat('mykey', 'field', '2.0e2')
+      .then(result => expect(result).toBe('5200'));
+  });
+});
