3ab97926c5b98cae92e3159fdba60f4a69171a37
Stian Didriksen <stipsan@me.com>
2016-10-02 03:30:47 +0200

    added `hincrbyfloat` command (#187)
    * added `hincrbyfloat` command
    
    * test coverage
    
    * fix test

---
+++ test/commands/hincrbyfloat.js
@@ -0,0 +1,45 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('hincrbyfloat', () => {
+  it('should increment an float with passed increment', () => {
+    const redis = new MockRedis({
+      data: {
+        mykey: { field: '10.50' },
+      },
+    });
+
+    return redis.hincrbyfloat('mykey', 'field', 0.1)
+      .then(result => expect(result).toBe('10.6'))
+      .then(() => redis.hincrbyfloat('mykey', 'field', -5))
+      .then(result => expect(result).toBe('5.6'));
+  });
+
+  it('should support exponents', () => {
+    const redis = new MockRedis({
+      data: {
+        mykey: { field: '5.0e3' },
+      },
+    });
+
+    return redis.hincrbyfloat('mykey', 'field', '2.0e2')
+      .then(result => expect(result).toBe('5200'));
+  });
+
+  it('should create hash if not exists', () => {
+    const redis = new MockRedis();
+
+    return redis.hincrbyfloat('stats', 'health', 0.5).then(result => expect(result).toBe('0.5'));
+  });
+
+  it('should create field in hash if not exists', () => {
+    const redis = new MockRedis({
+      data: {
+        stats: {},
+      },
+    });
+
+    return redis.hincrbyfloat('stats', 'health', 0.5).then(result => expect(result).toBe('0.5'));
+  });
+});
