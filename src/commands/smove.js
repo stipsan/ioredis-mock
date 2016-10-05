3937fb699718bb558f64fe38f557d1f37018cb3c
Stian Didriksen <stipsan@gmail.com>
2016-10-05 20:06:27 +0200

    adding SET command

---
+++ src/commands/smove.js
@@ -0,0 +1,29 @@
+import Set from 'es6-set';
+
+export function smove(source, destination, member) {
+  if (this.data.has(source) && !(this.data.get(source) instanceof Set)) {
+    throw new Error(`Source ${source} does not contain a set`);
+  }
+  if (this.data.has(destination) && !(this.data.get(destination) instanceof Set)) {
+    throw new Error(`Destination ${destination} does not contain a set`);
+  }
+
+  if (!this.data.has(source)) {
+    return 0;
+  }
+
+  const sourceSet = this.data.get(source);
+  if (!sourceSet.has(member)) {
+    return 0;
+  }
+
+  sourceSet.delete(member);
+
+  if (!this.data.has(destination)) {
+    this.data.set(destination, new Set());
+  }
+
+  this.data.get(destination).add(member);
+
+  return 1;
+}
--- test/commands/sadd.js
+++ test/commands/sadd.js
@@ -5,12 +5,14 @@ import MockRedis from '../../src';
 describe('sadd', () => {
   it('should add 1 item to set', () => {
     const redis = new MockRedis();
-    redis.sadd('foos', 'bar').then(status => expect(status).toBe(1))
+
+    return redis.sadd('foos', 'bar').then(status => expect(status).toBe(1))
       .then(() => expect(redis.data.get('foos').has('bar')).toBe(true));
   });
   it('should add 2 items to set', () => {
     const redis = new MockRedis();
-    redis.sadd('foos', 'foo', 'baz').then(status => expect(status).toBe(2))
+
+    return redis.sadd('foos', 'foo', 'baz').then(status => expect(status).toBe(2))
       .then(() => {
         expect(redis.data.get('foos').has('foo')).toBe(true);
         expect(redis.data.get('foos').has('baz')).toBe(true);
@@ -19,7 +21,8 @@ describe('sadd', () => {
   });
   it('should not increase length when adding duplicates', () => {
     const redis = new MockRedis();
-    redis.sadd('key', 'value', 'value')
+
+    return redis.sadd('key', 'value', 'value')
       .then(status => expect(status).toBe(1))
       .then(() => expect(redis.data.get('key').has('value')).toBe(true));
   });
