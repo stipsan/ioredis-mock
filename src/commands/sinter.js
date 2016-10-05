44cbcf5ed27ac151bf1d3bca36f5f4c6237ec876
Stian Didriksen <stipsan@gmail.com>
2016-10-05 21:51:03 +0200

    thanks codeclimate! I missed that one...

--- src/commands/sinter.js
+++ src/commands/sinter.js
@@ -6,7 +6,7 @@ import { sunion } from './index';
 export function sinter(...keys) {
   const values = sunion.apply(this, keys);
   const sets = keys.map(key => (this.data.has(key) ? this.data.get(key) : new Set()));
-  console.log(values);
+
   const intersection = new Set(values.filter(value => (
     sets.reduce((isShared, set) => (
       set.has(value) ? isShared : false
