0e8e7709b37eb958ac2a6497b7ea06cea2d2a8a0
David Pickart <davidstoreypickart@gmail.com>
2016-11-01 16:20:16 -0500

    Prevent conversion of buffer arguments to strings

--- test/command.js
+++ test/command.js
@@ -3,14 +3,19 @@ import expect, { createSpy } from 'expect';
 import command from '../src/command';
 
 describe('basic command', () => {
-  const stub = command(() => 'OK');
+  const stub = command((...args) => args);
   it('should return a Promise that resolves the returned value', () =>
-    stub().then(reply => expect(reply).toBe('OK'))
+    stub('OK').then(reply => expect(reply).toEqual(['OK']))
   );
 
   it('should support node style callbacks', () => {
     const spy = createSpy();
     return stub(spy).then(() => expect(spy).toHaveBeenCalled());
+  });
+
+  it('should convert non-buffer arguments to strings', () => {
+    const args = [new Buffer("foo"), "bar", 1]
+    return stub(...args).then(reply => expect(reply).toEqual([new Buffer("foo"), "bar", "1"]))
   });
 
   it('should reject the promise if the first argument is bool false to allow simulating failures');
