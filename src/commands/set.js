6943bc6de68ec9b8da6db2cccbc4fe49f77bee67
Edward Murphy <edward3murphy@gmail.com>
2017-03-07 16:11:23 -0500

    Update SET command with support for EX, PX, NX and XX

--- src/commands/set.js
+++ src/commands/set.js
@@ -1,5 +1,30 @@
-export function set(key, value) {
+import { expire } from './index';
+
+function createGroupedArray(arr, groupSize) {
+  const groups = [];
+  for (let i = 0; i < arr.length; i += groupSize) {
+    groups.push(arr.slice(i, i + groupSize));
+  }
+  return groups;
+}
+
+export function set(key, value, ...options) {
+  const nx = options.includes('NX');
+  const xx = options.includes('XX');
+  const filteredOptions = options.filter(option => option !== 'NX' && option !== 'XX');
+
+  if (nx && xx) return null;
+  if (nx && this.data.has(key)) return null;
+  if (xx && !this.data.has(key)) return null;
+
   this.data.set(key, value);
-  this.expires.delete(key);
+
+  const expireOptions = new Map(createGroupedArray(filteredOptions, 2));
+  const ttlSeconds = expireOptions.get('EX') || expireOptions.get('PX') / 1000.0;
+
+  if (ttlSeconds) {
+    expire.call(this, key, ttlSeconds);
+  }
+
   return 'OK';
 }
