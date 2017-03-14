03a25f1e4603cf0937008fe9ea41b82d287b69e5
edwmurph <edward3murphy@gmail.com>
2017-03-14 08:39:38 -0400

    Update SET command with support for EX, PX, NX and XX (#294)
    * Update SET command with support for EX, PX, NX and XX
    
    * remove key ttl if EX/PX not specified
    
    * Update SET: added unit test coverage and throw error if XX and NX are specified
    
    * improved UT coverage
    
    * update syntax to support older versions of node

--- src/commands/set.js
+++ src/commands/set.js
@@ -1,5 +1,32 @@
-export function set(key, value) {
+import { expire } from './index';
+
+function createGroupedArray(arr, groupSize) {
+  const groups = [];
+  for (let i = 0; i < arr.length; i += groupSize) {
+    groups.push(arr.slice(i, i + groupSize));
+  }
+  return groups;
+}
+
+export function set(key, value, ...options) {
+  const nx = options.indexOf('NX') !== -1;
+  const xx = options.indexOf('XX') !== -1;
+  const filteredOptions = options.filter(option => option !== 'NX' && option !== 'XX');
+
+  if (nx && xx) throw new Error('ERR syntax error');
+  if (nx && this.data.has(key)) return null;
+  if (xx && !this.data.has(key)) return null;
+
   this.data.set(key, value);
-  this.expires.delete(key);
+
+  const expireOptions = new Map(createGroupedArray(filteredOptions, 2));
+  const ttlSeconds = expireOptions.get('EX') || expireOptions.get('PX') / 1000.0;
+
+  if (ttlSeconds) {
+    expire.call(this, key, ttlSeconds);
+  } else {
+    this.expires.delete(key);
+  }
+
   return 'OK';
 }
