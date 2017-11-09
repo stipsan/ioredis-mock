9bf96088da733776fab053d6afcb97bca5518e4d
Tyler Wray <wraytw@users.noreply.github.com>
2017-11-09 09:47:37 -0700

    Adding edge cases to hexists

--- src/commands/hexists.js
+++ src/commands/hexists.js
@@ -1,3 +1,7 @@
 export function hexists(key, field) {
-  return {}.hasOwnProperty.call(this.data.get(key), field) ? 1 : 0;
+  const hash = this.data.get(key)
+  if (!hash || hash[field] === undefined) {
+    return 0;
+  }
+  return {}.hasOwnProperty.call(hash, field) ? 1 : 0;
 }
