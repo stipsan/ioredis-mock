b7521525e328a5104bb8a032c16a622e250bbcc6
Stian Didriksen <stipsan@gmail.com>
2017-11-28 02:58:10 +0100

    Fix: More detailed RunKit example (#346)
    * Chore: Update feature compat table
    
    * Fix: Update example on RunKit
    
    * Chore: Update feature compat table
    
    * Chore: Update feature compat table
    
    * Chore: Update feature compat table
    
    * better yet

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
-// peer dependency
-require('ioredis/package.json')
+// ioredis is a peer dependency so we must include it for the RunKit example to work
+require('ioredis/package.json');
 
 var RedisMock = require('ioredis-mock');
 var redis = new RedisMock({
@@ -7,11 +7,20 @@ var redis = new RedisMock({
     user_next: '3',
     emails: {
       'clark@daily.planet': '1',
-      'bruce@wayne.enterprises': '2',
+      'bruce@wayne.enterprises': '2'
     },
     'user:1': { id: '1', username: 'superman', email: 'clark@daily.planet' },
-    'user:2': { id: '2', username: 'batman', email: 'bruce@wayne.enterprises' },
-  },
+    'user:2': { id: '2', username: 'batman', email: 'bruce@wayne.enterprises' }
+  }
 });
 
-await redis.incr('user_next');
+const userNext = await redis.incr('user_next');
+await redis.hmset(
+  `user:${userNext}`,
+  new Map([
+    ['id', userNext],
+    ['username', 'wonderwoman'],
+    ['email', 'diana@amazon.gr']
+  ])
+);
+await redis.hgetall(`user:${userNext}`);
