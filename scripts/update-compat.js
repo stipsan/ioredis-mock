f310658fdaa92b4b2aedd3ebdb699be16f9baa54
PrettierCI <commits@prettierci.com>
2018-03-13 13:33:38 +0000

    Sync with Prettier

--- scripts/update-compat.js
+++ scripts/update-compat.js
@@ -28,9 +28,7 @@ let tableRows = `
 | redis | ioredis | ioredis-mock |
 |-------|:-------:|:------------:|`;
 filteredCommands.forEach(command => {
-  const redisCol = `[${
-    command
-  }](http://redis.io/commands/${command.toUpperCase()})`;
+  const redisCol = `[${command}](http://redis.io/commands/${command.toUpperCase()})`;
   const ioredisCol = command in redis.prototype ? ':white_check_mark:' : ':x:';
   const supportedCommand = command in mockedRedis;
   const ioredisMockCol = supportedCommand ? ':white_check_mark:' : ':x:';
@@ -62,9 +60,7 @@ if (percentage === 100) {
   color = 'brightgreen';
 }
 
-const tableMd = `## Supported commands ![Commands Coverage: ${
-  percentage
-}%](https://img.shields.io/badge/coverage-${percentage}%25-${color}.svg)
+const tableMd = `## Supported commands ![Commands Coverage: ${percentage}%](https://img.shields.io/badge/coverage-${percentage}%25-${color}.svg)
 ${tableRows}`;
 
 fs.writeFile(
@@ -86,11 +82,7 @@ fs.readFile(readme, 'utf8', (err, readmeMd) => {
       .toString()
       .replace(
         /\[!\[Redis.+\]\(compat\.md\)/g,
-        `[![Redis Compatibility: ${
-          percentage
-        }%](https://img.shields.io/badge/redis-${percentage}%25-${
-          color
-        }.svg?style=flat-square)](compat.md)`
+        `[![Redis Compatibility: ${percentage}%](https://img.shields.io/badge/redis-${percentage}%25-${color}.svg?style=flat-square)](compat.md)`
       ),
     'utf8',
     err2 => {
