6d6fa3756800678707b35ba3fd71a4b492ebadd3
Bastien Arata <bastien.arata@sigfox.com>
2018-03-29 15:37:50 +0200

    :sparkles: add scanStream command

---
+++ src/commands-stream/scanStream.js
@@ -0,0 +1,5 @@
+import ReadableStream from '../commands-utils/readable-scan';
+
+export function scanStream(opt) {
+  return new ReadableStream(this.scan, opt);
+}
---
+++ src/commands-utils/readable-scan.js
@@ -0,0 +1,43 @@
+import { Readable } from 'stream';
+
+export default class ReadableScan extends Readable {
+  constructor(scanCommand, opt = {}) {
+    super({ objectMode: true });
+    this._scanCommand = scanCommand;
+    this._cursor = 0;
+    this.opt = opt;
+    this._drained = false;
+  }
+
+  _callScan() {
+    const args = [this._cursor];
+    if (this.opt.key) {
+      args.unshift(this.opt.key);
+    }
+    if (this.opt.match) {
+      args.push('MATCH', this.opt.match);
+    }
+    if (this.opt.count) {
+      args.push('COUNT', this.opt.count);
+    }
+    return this._scanCommand(...args);
+  }
+
+  _read() {
+    if (this._drained) {
+      this.push(null);
+      return;
+    }
+    this._callScan()
+      .then(res => {
+        const [nextCursor, keys] = res;
+        if (nextCursor === 0) {
+          this._drained = true;
+        } else {
+          this._cursor = nextCursor;
+        }
+        this.push(keys);
+      })
+      .catch(err => process.nextTick(() => this.emit('error', err)));
+  }
+}
