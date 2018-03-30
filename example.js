75e44d6d619334f0857d2fea98e2049c90c7d4ad
Stian Didriksen <stipsan@gmail.com>
2018-03-30 18:13:48 +0200

    formatted with prettier

--- example.js
+++ example.js
@@ -7,24 +7,24 @@ var redis = new RedisMock({
     user_next: '3',
     emails: {
       'clark@daily.planet': '1',
-      'bruce@wayne.enterprises': '2'
+      'bruce@wayne.enterprises': '2',
     },
     'user:1': { id: '1', username: 'superman', email: 'clark@daily.planet' },
-    'user:2': { id: '2', username: 'batman', email: 'bruce@wayne.enterprises' }
-  }
+    'user:2': { id: '2', username: 'batman', email: 'bruce@wayne.enterprises' },
+  },
 });
 
 async function main() {
-const userNext = await redis.incr('user_next');
-await redis.hmset(
-  `user:${userNext}`,
-  new Map([
-    ['id', userNext],
-    ['username', 'wonderwoman'],
-    ['email', 'diana@amazon.gr']
-  ])
-);
-console.log(await redis.hgetall(`user:${userNext}`));
+  const userNext = await redis.incr('user_next');
+  await redis.hmset(
+    `user:${userNext}`,
+    new Map([
+      ['id', userNext],
+      ['username', 'wonderwoman'],
+      ['email', 'diana@amazon.gr'],
+    ])
+  );
+  console.log(await redis.hgetall(`user:${userNext}`));
 }
 
-main()
+main();
