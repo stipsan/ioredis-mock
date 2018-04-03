2775f6962d67fd4d65d18e783491c519e9d41c53
Oleg Vaskevich <oleg@osv.im>
2018-04-03 04:25:13 -0400

    Fix: `pexpire, pexpireat, psetex` to set correct expire time (#389 @vaskevich)

--- test/commands/psetex.js
+++ test/commands/psetex.js
@@ -1,4 +1,5 @@
 import expect from 'expect';
+import Promise from 'bluebird';
 
 import MockRedis from '../../src';
 
@@ -6,11 +7,14 @@ describe('psetex', () => {
   it('should set value and expire', () => {
     const redis = new MockRedis();
     return redis
-      .psetex('foo', 1000, 'bar')
+      .psetex('foo', 100, 'bar')
       .then(status => expect(status).toBe('OK'))
       .then(() => {
         expect(redis.data.get('foo')).toBe('bar');
         expect(redis.expires.has('foo')).toBe(true);
-      });
+      })
+      .then(() => Promise.delay(200))
+      .then(() => redis.get('foo'))
+      .then(result => expect(result).toBe(null));
   });
 });
