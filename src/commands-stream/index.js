6c20f05106a333715cc9a998972b207b0273ae44
Bastien Arata <banst@Bastiens-MacBook-Pro.local>
2018-04-04 11:19:59 +0200

    :sparkles: add sscanStream command

--- src/commands-stream/index.js
+++ src/commands-stream/index.js
@@ -1,1 +1,2 @@
 export * from './scanStream';
+export * from './sscanStream';
---
+++ src/commands-stream/sscanStream.js
@@ -0,0 +1,7 @@
+import ReadableStream from '../commands-utils/readable-scan';
+
+export function sscanStream(key, opt = {}) {
+  const options = opt instanceof Object ? opt : {};
+  options.key = key;
+  return new ReadableStream(this.sscan, options);
+}
