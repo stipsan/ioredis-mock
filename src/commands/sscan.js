934b269c3c08be687a290049163deb701028ac55
Bastien Arata <banst@Bastiens-MacBook-Pro.local>
2018-04-04 12:30:23 +0200

    :green_heart: fallback to good old foreach for node 0.12 compatibility
    👴

--- src/commands/sscan.js
+++ src/commands/sscan.js
@@ -4,6 +4,7 @@ export function sscan(key, cursor, ...args) {
   if (!this.data.has(key)) {
     return [0, []];
   }
-  const setKeys = [...this.data.get(key).values()];
+  const setKeys = [];
+  this.data.get(key).forEach(value => setKeys.push(value));
   return scanHelper(setKeys, 1, cursor, ...args);
 }
