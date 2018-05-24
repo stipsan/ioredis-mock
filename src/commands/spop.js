fff71a8d5cc06e1564e044ff1a94027fd7e90408
Bastien Arata <bastyen.a@gmail.com>
2018-05-24 16:45:02 +0200

    :bug: fix var usage

--- src/commands/spop.js
+++ src/commands/spop.js
@@ -1,18 +1,21 @@
 import _ from 'lodash';
 import Set from 'es6-set';
 
+const safeCount = count => {
+  const result = count !== undefined ? parseInt(count, 10) : 1;
+  if (Number.isNaN(result) || result < 0) {
+    throw new Error('ERR value is not an integer or out of range');
+  }
+  return result;
+};
+
 export function spop(key, count) {
   if (this.data.has(key) && !(this.data.get(key) instanceof Set)) {
     throw new Error(`Key ${key} does not contain a set`);
   }
-
+  const want = safeCount(count);
   const set = this.data.get(key) || new Set();
   const total = set.size;
-  const want = count !== undefined ? parseInt(count, 10) : 1;
-
-  if (Number.isNaN(count) || count < 0) {
-    throw new Error('ERR value is not an integer or out of range');
-  }
 
   if (want === 0) return undefined;
   if (total === 0) return null;
