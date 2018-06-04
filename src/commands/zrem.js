533f3f6a322ce7fa6b06b538d2bc32925445519d
Kyle Mahan <kyle.mahan@gmail.com>
2018-06-04 11:07:28 -0700

    Feat: add zrem and zincrby (#443)

---
+++ src/commands/zrem.js
@@ -0,0 +1,14 @@
+export function zrem(key, ...vals) {
+  const map = this.data.get(key);
+  if (!map) return 0;
+
+  let removed = 0;
+  vals.forEach(val => {
+    if (map.delete(val)) {
+      removed++;
+    }
+  });
+
+  this.data.set(key, map);
+  return removed;
+}
---
+++ test/commands/zincrby.js
@@ -0,0 +1,37 @@
+import expect from 'expect';
+
+import MockRedis from '../../src';
+
+describe('zincrby', () => {
+  const data = {
+    foos: new Map([
+      ['foo', { value: 'foo', score: 1 }],
+      ['bar', { value: 'bar', score: 2 }],
+      ['baz', { value: 'baz', score: 3 }],
+    ]),
+  };
+  it('should increment the score of an item in a sorted set', () => {
+    const redis = new MockRedis({ data });
+    return redis
+      .zincrby('foos', 10, 'foo')
+      .then(status => expect(status).toBe('11'))
+      .then(() =>
+        expect(redis.data.get('foos').get('foo')).toEqual({
+          value: 'foo',
+          score: 11,
+        })
+      );
+  });
+  it('should initialize a non-existent key', () => {
+    const redis = new MockRedis({ data });
+    return redis
+      .zincrby('foos', 4, 'qux')
+      .then(status => expect(status).toBe('4'))
+      .then(() => {
+        expect(redis.data.get('foos').get('qux')).toEqual({
+          value: 'qux',
+          score: 4,
+        });
+      });
+  });
+});
