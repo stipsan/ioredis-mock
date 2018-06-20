c2edf90ad9ff1871fb23a22aad528a4d92b309ac
Christo <christo@cryptodrunks.net>
2018-06-20 10:56:51 +0200

    Feat: add xadd, xlen, xrange, xrevrange and xread for stream support
    Support basic stream operations (https://redis.io/topics/streams-intro) for
    the upcoming Redis 5 release. Some features are missing, such as consumer
    groups, capped streams or some of the auxiliary commands.
    
    Event ids are auto incremented integers in the form of `1-0`, `2-0`,
    etc. Events are stored internally as an array of arrays and for every event an
    additional record is kept whether the event has been already polled or not.
    
    ```
    const redis = new MockRedis({
      mystream: [
        ["1-0", ["key1", "val1", "key2", "val2"]],
        ["2-0", ["key1", "val1", "key2", "val2"]]
      ],
      "stream:mystream:1-0": {polled: false},
      "stream:mystream:2-0": {polled: false},
    })
    ```
    
    To poll events in a blocking manner is supported and the poll happens every
    100ms.
    
    ```
    // This will resolve once a new event becomes available.
    redis.xread("BLOCK", "0", "STREAMS", "mystream", "$").then(console.log);
    // After 1 second add a new event, that will resolve the waiting `XREAD`
    // command.
    setTimeout(() => redis.xadd("mystream", "*", "key", "val"), 1000);
    ```

---
+++ src/commands/xread.js
@@ -0,0 +1,68 @@
+export function xread(option, ...args) {
+  const pollStream = (stream, id, count = 1) => {
+    const data = this.data.get(stream);
+    if (!data) return [];
+    return data.reduce((memo, [eventId, ...row]) => {
+      const { polled } = this.data.get(`stream:${stream}:${eventId}`);
+      if (!polled && (id === '$' || eventId >= id) && memo.length < count) {
+        this.data.set(`stream:${stream}:${eventId}`, { polled: true });
+        return [[stream, [eventId, ...row]]].concat(memo);
+      }
+      return memo;
+    }, []);
+  };
+
+  const { op, opVal, rest } =
+    option === 'STREAMS'
+      ? { op: 'COUNT', opVal: Infinity, rest: args }
+      : {
+          op: option,
+          opVal: parseInt(args[0], 10),
+          rest: args.slice(2),
+        };
+
+  if (!['COUNT', 'BLOCK'].includes(op)) {
+    throw new Error('ERR syntax error');
+  }
+
+  if (rest.length % 2 !== 0) {
+    throw new Error(
+      "ERR Unbalanced XREAD list of streams: for each stream key an ID or '$' must be specified."
+    );
+  }
+
+  // Turn ["stream1", "stream2", "id1", "id2"] into tuples of
+  //      [["stream1", "id1"], ["stream2", "id2"]]
+  const toPoll = rest.reduce((memo, arg, i) => {
+    const chunk = Math.floor(i / 2);
+    const tuple = memo[chunk] || [];
+    // eslint-disable-next-line no-param-reassign
+    memo[chunk] = tuple.concat(arg);
+    return memo;
+  }, []);
+
+  const pollEvents = (streams, countVal) =>
+    streams.reduce(
+      (memo, [stream, id]) => pollStream(stream, id, countVal).concat(memo),
+      []
+    );
+
+  return op === 'BLOCK'
+    ? new Promise(resolve => {
+        let timeElapsed = 0;
+        const f = () =>
+          setTimeout(() => {
+            if (opVal > 0 && timeElapsed < opVal) return resolve(null);
+            const events = pollEvents(toPoll, 1);
+            if (events.length > 0) return resolve(events);
+            timeElapsed += 100;
+            return f();
+          }, 100);
+        f();
+      })
+    : new Promise(resolve => {
+        const events = pollEvents(toPoll, opVal);
+        if (events.length === 0) return resolve(null);
+        return resolve(events.slice().reverse());
+      });
+}
