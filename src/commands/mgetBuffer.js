8f2c66a210f2d5dba108bdbe12e710470bbd3b7c
Artyom Suchkov <fanasew@gmail.com>
2019-08-09 13:48:50 +0200

    mgetBuffer.js created

---
+++ src/commands/mgetBuffer.js
@@ -0,0 +1,7 @@
+import { mget } from './hmget';
+import createBuffer from '../buffer';
+
+export function mgetBuffer(key, ...fields) {
+  const val = mget.apply(this, [key, ...fields]);
+  return val.map(payload => (payload ? createBuffer(payload) : payload));
+}
