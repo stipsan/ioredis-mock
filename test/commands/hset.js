0205c296e63220beab9d20c8aa48f594f60c343d
Nick S <nskg405@gmail.com>
2019-12-04 17:46:58 -0800

    changed hset functionality to act as hmset

--- test/commands/hset.js
+++ test/commands/hset.js
@@ -24,4 +24,20 @@ describe('hset', () => {
           email: 'bruce@wayne.enterprises',
         })
       ));
+
+  it('should let you set multiple hash map keys and values in a single command', () => {
+    const hash = { id: '2', email: 'bruce@wayne.enterprises' };
+    return redis
+      .hset('user:2', 'id', '2', 'email', 'bruce@wayne.enterprises')
+      .then(status => expect(status).toBe(2))
+      .then(() => expect(redis.data.get('user:2')).toEqual(hash));
+  });
+
+  it('should let you set multiple hash map keys and values with an object', () => {
+    const hash = { id: '3', email: 'bruce@wayne.enterprises' };
+    return redis
+      .hset('user:3', hash)
+      .then(status => expect(status).toBe(1))
+      .then(() => expect(redis.data.get('user:3')).toEqual(hash));
+  });
 });
