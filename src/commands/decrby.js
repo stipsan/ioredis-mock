e31c9483d12532f319e9ef72e823b604ede7c963
Doug Ayers <4142577+douglascayers@users.noreply.github.com>
2020-08-13 17:40:00 -0500

    fix: default set key-value to 0 if does not exist
    https://redis.io/commands/decrby
    
    fixes #877

--- src/commands/decrby.js
+++ src/commands/decrby.js
@@ -1,4 +1,7 @@
 export function decrby(key, decrement = 0) {
+  if (!this.data.has(key)) {
+    this.data.set(key, '0');
+  }
   const curVal = Number(this.data.get(key));
   const nextVal = curVal - parseInt(decrement, 10);
   this.data.set(key, nextVal.toString());
