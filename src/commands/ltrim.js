3bb321b122946bf865e0d7c9fb3cadee52134095
David Case <david@kronoverse.io>
2020-09-05 16:09:35 -0400

    Fix ltrim where negative start/end trims imporperly

--- src/commands/ltrim.js
+++ src/commands/ltrim.js
@@ -2,18 +2,11 @@ export function ltrim(key, s, e) {
   if (this.data.has(key) && !(this.data.get(key) instanceof Array)) {
     throw new Error(`Key ${key} does not contain a list`);
   }
-  let start = parseInt(s, 10);
-  let end = parseInt(e, 10);
+  const start = parseInt(s, 10);
+  const end = parseInt(e, 10);
 
   const list = this.data.get(key) || [];
 
-  if (start < 0) {
-    start = list.length + start;
-  }
-  if (end < 0) {
-    end = list.length + end;
-  }
-
-  this.data.set(key, list.slice(start, end + 1));
+  this.data.set(key, list.slice(start, end + 1 || undefined));
   return 'OK';
 }
