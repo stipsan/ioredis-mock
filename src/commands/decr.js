9f740f3698536ebbd106e1ea48d5b22b471c9199
Doug Ayers <4142577+douglascayers@users.noreply.github.com>
2020-09-30 12:02:38 -0500

    fix: Fixes #877 DECR and DECRBY defaults value if key doesn't exist (#969)
    * fix: default set key-value to 0 if does not exist
    
    https://redis.io/commands/decr
    
    fixes #877
    
    * fix: default set key-value to 0 if does not exist
    
    https://redis.io/commands/decrby
    
    fixes #877

--- src/commands/decr.js
+++ src/commands/decr.js
@@ -1,4 +1,7 @@
 export function decr(key) {
+  if (!this.data.has(key)) {
+    this.data.set(key, '0');
+  }
   const curVal = Number(this.data.get(key));
   const nextVal = curVal - 1;
   this.data.set(key, nextVal.toString());
