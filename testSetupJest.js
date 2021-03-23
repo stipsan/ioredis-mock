df1631c7b800b6adfe011f3e153aa5b66502298f
Cody Olsen <stipsan@gmail.com>
2021-03-23 15:55:32 +0000

    chore: eslint strikes again

--- testSetupJest.js
+++ testSetupJest.js
@@ -1,4 +1,4 @@
 // Redirects 'ioredis' imports to the compiled jest.js file that inlines the Command API from 'ioredis'
 
-// eslint-disable-next-line global-require
+// eslint-disable-next-line global-require, import/no-unresolved
 jest.mock('ioredis', () => require('./jest'));
