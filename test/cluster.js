80db0e713647d427cbf5a1f4a1cba4505498edda
onichandame <zxinmyth@gmail.com>
2021-04-15 07:35:11 +0000

    fix lint in test

--- test/cluster.js
+++ test/cluster.js
@@ -1,13 +1,13 @@
 import RedisMock from '../src';
 
-describe(`cluster`, () => {
-  it(`can create instance`, () => {
-    const cluster = new RedisMock.Cluster([`redis://localhost:6379`]);
+describe('cluster', () => {
+  it('can create instance', () => {
+    const cluster = new RedisMock.Cluster(['redis://localhost:6379']);
     return expect(cluster instanceof RedisMock.Cluster).toBeTruthy();
   });
 
-  it(`can return correct number of nodes`, () => {
-    const nodes = [`redis://localhost:7001`, `redis://localhost:7002`];
+  it('can return correct number of nodes', () => {
+    const nodes = ['redis://localhost:7001', 'redis://localhost:7002'];
     const cluster = new RedisMock.Cluster(nodes);
     expect(cluster.nodes.length).toEqual(nodes.length);
     expect(
@@ -15,11 +15,11 @@ describe(`cluster`, () => {
     ).toBeTruthy();
   });
 
-  it(`can set and get`, () => {
-    const cluster = new RedisMock.Cluster([``]);
+  it('can set and get', () => {
+    const cluster = new RedisMock.Cluster(['']);
     return cluster
-      .set(`k`, `v`)
-      .then(() => cluster.get(`k`))
-      .then((v) => expect(v).toEqual(`v`));
+      .set('k', 'v')
+      .then(() => cluster.get('k'))
+      .then((v) => expect(v).toEqual('v'));
   });
 });
