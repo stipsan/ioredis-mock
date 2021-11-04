b6170dab9c93f5a2d7da718562d8c5c069e10fdb
T.J. Tarazevits <tj@cameo.com>
2021-11-04 09:38:16 -0700

    Fix: (SMISMEMBER) fix block linter errors

--- src/commands/smismember.js
+++ src/commands/smismember.js
@@ -2,9 +2,7 @@ export function smismember(key, ...valArray) {
   const data = this.data.get(key);
 
   if (data) {
-    return valArray.map((val) => {
-      return data.has(val) ? 1 : 0;
-    })
+    return valArray.map((val) => data.has(val) ? 1 : 0);
   }
-  return valArray.map((val) => 0);
+  return valArray.map(() => 0);
 }
