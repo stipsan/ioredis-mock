d88c8bbaac2eaac107a8eefbf8cdd8b2711c8f9a
Fernando Silva <fernando457829@gmail.com>
2022-01-21 15:08:58 -0300

    fix: command transformers

--- testSetupJest.js
+++ testSetupJest.js
@@ -1,4 +1,13 @@
 // Redirects 'ioredis' imports to the compiled jest.js file that inlines the Command API from 'ioredis'
 
 // eslint-disable-next-line global-require, import/no-unresolved
-jest.mock('ioredis', () => require('./jest'));
+jest.mock('ioredis', () => {
+  const { Command } = jest.requireActual('ioredis');
+  const Redis = jest.requireActual('./jest');
+
+  return {
+    __esModule: true,
+    Command,
+    default: Redis,
+  };
+});
