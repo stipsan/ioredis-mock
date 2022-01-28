903c4189ad377f69aa4bbadcbafac02bcbdb8539
Cody Olsen <stipsan@gmail.com>
2022-01-28 03:14:38 +0100

    only run integration tests e2e

--- jest.config.redis.js
+++ jest.config.redis.js
@@ -1,5 +1,6 @@
 module.exports = {
   // eslint-disable-next-line global-require
   ...require('./jest.config'),
+  testMatch: ['**/test/integration/**/*.js'],
   setupFiles: ['<rootDir>/testSetupRedis.js'],
 };
