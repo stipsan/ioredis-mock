f44ece313aa4ce83cf1678ce702ecd058e05f19d
Cody Olsen <stipsan@gmail.com>
2022-01-28 02:46:10 +0100

    Update hset.js

--- test/integration/commands/hset.js
+++ test/integration/commands/hset.js
@@ -13,18 +13,34 @@ describe('hset', () => {
         })
       ));
 
-  it('should return 0 when overwriting existing field', () =>
+      it('should return 2 when setting a 2 fields', () =>
     redis
+      .hset('user:1', 'email', 'bruce@wayne.enterprises', 'age', '24')
+      .then((status) => expect(status).toBe(2))
+      .then(() =>
+        expect(redis.data.get('user:1')).toEqual({
+          email: 'bruce@wayne.enterprises',
+          age: '24',
+        })
+      ));
+
+  it('should return 0 when overwriting existing field', async () =>
+    {
+      await redis
+      .hset('user:1', 'email', 'clark@daily.planet');return redis
       .hset('user:1', 'email', 'bruce@wayne.enterprises')
       .then((status) => expect(status).toBe(0))
       .then(() =>
         expect(redis.data.get('user:1')).toEqual({
           email: 'bruce@wayne.enterprises',
         })
-      ));
+      )});
 
-  it('should allo setting multiple fields', () =>
-    redis
+  it('should allow setting multiple fields', async () =>
+    {
+      await redis
+      .hset('user:1', 'email', 'clark@daily.planet');
+      return redis
       .hset('user:1', 'email', 'bruce@wayne.enterprises', 'age', '24')
       .then((status) => expect(status).toBe(1))
       .then(() =>
@@ -32,5 +48,6 @@ describe('hset', () => {
           email: 'bruce@wayne.enterprises',
           age: '24',
         })
-      ));
+      )});
+      
 });
