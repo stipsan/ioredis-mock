f1e915a1e17d7dfd45aedee6a6df34aad6f19c20
Cody Olsen <stipsan@gmail.com>
2022-01-28 18:02:16 +0100

    chore: rename default branch to main (#1128)

--- test/functional/commands.js
+++ test/functional/commands.js
@@ -5,14 +5,14 @@ describe('commands', () => {
   test('Calling unsupported command throws a descriptive error', () => {
     const redis = new Redis()
     expect(() => redis.object()).toThrowErrorMatchingInlineSnapshot(
-      `"Unsupported command: \\"object\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/master/compat.md#supported-commands-"`
+      `"Unsupported command: \\"object\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
     )
   })
 
   test('Calling unsupported buffer command throws a descriptive error', () => {
     const redis = new Redis()
     expect(() => redis.objectBuffer()).toThrowErrorMatchingInlineSnapshot(
-      `"Unsupported command: \\"objectBuffer\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/master/compat.md#supported-commands-"`
+      `"Unsupported command: \\"objectBuffer\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
     )
   })
 })
