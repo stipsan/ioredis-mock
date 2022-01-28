1dacde490f56730664c07eb9e5c569467c2ba23b
Cody Olsen <stipsan@gmail.com>
2022-01-28 04:29:33 +0100

    setting up tests

--- test/integration/commands/zadd.js
+++ test/integration/commands/zadd.js
@@ -1,78 +1,60 @@
 import Redis from 'ioredis';
 
 describe('zadd', () => {
-  it('should add 1 item to sorted set', () => {
-    const redis = new Redis();
+  const redis = new Redis();
 
-    return redis
-      .zadd('foos', '1', 'foo')
-      .then((status) => expect(status).toBe(1))
-      .then(() => expect(redis.data.get('foos').has('foo')).toBe(true));
+  afterAll(() => {return redis.disconnect()});
+
+  it('should add 1 item to sorted set', async () => {
+    const status = await redis.zadd('foos', '1', 'foo');
+    expect(status).toBe(1);
+
+    expect(await redis.zrange('foos', 0, -1)).toEqual(['foo']);
   });
-  it('should add 2 items to sorted set', () => {
-    const redis = new Redis();
+  it('should add 2 items to sorted set', async () => {
+    const status = await redis.zadd('foos', '1', 'foo', '2', 'baz');
+    expect(status).toBe(2);
 
-    return redis
-      .zadd('foos', '1', 'foo', '2', 'baz')
-      .then((status) => expect(status).toBe(2))
-      .then(() => {
-        expect(redis.data.get('foos').has('foo')).toBe(true);
-        expect(redis.data.get('foos').has('baz')).toBe(true);
-        expect(redis.data.get('foos').has('bar')).toBe(false);
-      });
+    expect(await redis.zrange('foos', 0, -1)).toEqual(['foo', 'baz']);
   });
-  it('should not increase length when adding duplicates', () => {
-    const redis = new Redis();
-
-    return redis
+  it('should not increase length when adding duplicates', () =>
+    {return  redis
       .zadd('key', 'value', 'value')
-      .then((status) => expect(status).toBe(1))
-      .then(() => expect(redis.data.get('key').has('value')).toBe(true));
-  });
-  it('should not allow nx and xx options in the same call', () => {
-    const redis = new Redis();
-
-    return redis
+      .then((status) => {return expect(status).toBe(1)})
+      .then(() => {return expect(redis.data.get('key').has('value')).toBe(true)})});
+  it('should not allow nx and xx options in the same call', () =>
+    {return redis
       .zadd('key', ['NX', 'XX'], 1, 'value')
       .catch((e) =>
-        expect(e.message).toEqual(
+        {return expect(e.message).toEqual(
           'XX and NX options at the same time are not compatible'
-        )
-      );
-  });
+        )}
+      )});
   it('should not update a value that exists with NX option', () => {
-    const redis = new Redis();
-
     redis.zadd('key', 'NX', 1, 'value').then(() => {
-      redis.zadd('key', 'NX', 2, 'value').then((r) => expect(r).toBe(0));
+      redis.zadd('key', 'NX', 2, 'value').then((r) => {return expect(r).toBe(0)});
     });
   });
   it('should return updated + added with CH option', () => {
-    const redis = new Redis();
-
     redis.zadd('key', 1, 'value').then(() => {
       redis
         .zadd('key', 'CH', 3, 'value', 2, 'value2')
-        .then((r) => expect(r).toBe(2));
+        .then((r) => {return expect(r).toBe(2)});
     });
   });
   it('should only update elements that already exist with XX option', () => {
-    const redis = new Redis();
-
     redis.zadd('key', 1, 'value').then(() => {
       redis
         .zadd('key', ['XX', 'CH'], 2, 'value')
-        .then((r) => expect(r).toBe(1));
+        .then((r) => {return expect(r).toBe(1)});
     });
   });
   it('should handle INCR option', () => {
-    const redis = new Redis();
-
     redis.zadd('key', 1, 'value').then(() => {
       redis
         .zadd('key', 'INCR', 2, 'value')
         .then(() =>
-          expect(redis.data.get('key').get('value').score).toEqual(3)
+          {return expect(redis.data.get('key').get('value').score).toEqual(3)}
         );
     });
   });
