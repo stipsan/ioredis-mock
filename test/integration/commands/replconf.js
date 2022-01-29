e85a1e42531f204a3ef8f8069f9f6f4825b7a9be
Cody Olsen <stipsan@gmail.com>
2022-01-29 01:42:52 +0100

    add replconf

---
+++ test/integration/commands/replconf.js
@@ -0,0 +1,13 @@
+import Redis from 'ioredis'
+
+// eslint-disable-next-line import/no-relative-parent-imports
+import { runTwinSuite } from '../../../test-utils'
+
+runTwinSuite('replconf', (command, equals) => {
+  describe(command, () => {
+    it('should return OK', async () => {
+      const redis = new Redis()
+      expect(equals(await redis[command](), 'OK')).toBe(true)
+    })
+  })
+})
