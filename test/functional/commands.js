ab697ac3eeb6bc78335a20c004da14e35573d60a
Cody Olsen <stipsan@gmail.com>
2022-01-31 02:54:15 +0100

    fix: the object command is no longer unsupported

--- test/functional/commands.js
+++ test/functional/commands.js
@@ -4,15 +4,15 @@ import Redis from 'ioredis'
 describe('commands', () => {
   test('Calling unsupported command throws a descriptive error', () => {
     const redis = new Redis()
-    expect(() => redis.object()).toThrowErrorMatchingInlineSnapshot(
-      `"Unsupported command: \\"object\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
+    expect(() => redis.unsupported()).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported command: \\"unsupported\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
     )
   })
 
   test('Calling unsupported buffer command throws a descriptive error', () => {
     const redis = new Redis()
-    expect(() => redis.objectBuffer()).toThrowErrorMatchingInlineSnapshot(
-      `"Unsupported command: \\"objectBuffer\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
+    expect(() => redis.unsupportedBuffer()).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported command: \\"unsupportedBuffer\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
     )
   })
 })
