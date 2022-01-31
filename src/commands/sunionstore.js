5ef34496904313806dc7ba1ef4833a9e3ea3ba33
Cody Olsen <stipsan@gmail.com>
2022-01-31 07:48:29 +0100

    feat: add sunionstoreBuffer

--- src/commands/sunionstore.js
+++ src/commands/sunionstore.js
@@ -2,3 +2,5 @@ import { makeStoreSetCommand } from '../commands-utils/makeStoreCommand'
 import { sunion } from './index'
 
 export const sunionstore = makeStoreSetCommand(sunion)
+
+export const sunionstoreBuffer = sunionstore
--- test/integration/commands/sunionstore.js
+++ test/integration/commands/sunionstore.js
@@ -1,39 +1,43 @@
 import Redis from 'ioredis'
 
-describe('sunionstore', () => {
-  it('should store the union between the first set and all the successive sets at dest', () => {
-    const redis = new Redis({
-      data: {
-        key1: new Set(['a', 'b', 'c', 'd']),
-        key2: new Set(['c']),
-        // key3: keys that do not exist are considered to be empty sets
-        key4: new Set(['a', 'c', 'e']),
-      },
+// eslint-disable-next-line import/no-relative-parent-imports
+import { runTwinSuite } from '../../../test-utils'
+
+runTwinSuite('sunionstore', command => {
+  describe(command, () => {
+    it('should store the union between the first set and all the successive sets at dest', () => {
+      const redis = new Redis({
+        data: {
+          key1: new Set(['a', 'b', 'c', 'd']),
+          key2: new Set(['c']),
+          // key3: keys that do not exist are considered to be empty sets
+          key4: new Set(['a', 'c', 'e']),
+        },
+      })
+
+      return redis[command]('dest', 'key1', 'key2', 'key3', 'key4')
+        .then(count => {
+          return expect(count).toEqual(5)
+        })
+        .then(() => {
+          return redis.smembers('dest')
+        })
+        .then(result => {
+          return expect(result).toEqual(['a', 'b', 'c', 'd', 'e'])
+        })
     })
 
-    return redis
-      .sunionstore('dest', 'key1', 'key2', 'key3', 'key4')
-      .then(count => {
-        return expect(count).toEqual(5)
+    it('should throw an exception if one of the keys is not a set', () => {
+      const redis = new Redis({
+        data: {
+          foo: new Set(),
+          bar: 'not a set',
+        },
       })
-      .then(() => {
-        return redis.smembers('dest')
-      })
-      .then(result => {
-        return expect(result).toEqual(['a', 'b', 'c', 'd', 'e'])
-      })
-  })
 
-  it('should throw an exception if one of the keys is not a set', () => {
-    const redis = new Redis({
-      data: {
-        foo: new Set(),
-        bar: 'not a set',
-      },
+      return expect(redis[command]('foo', 'bar')).rejects.toEqual(
+        Error('Key bar does not contain a set')
+      )
     })
-
-    return expect(redis.sunionstore('foo', 'bar')).rejects.toEqual(
-      Error('Key bar does not contain a set')
-    )
   })
 })
