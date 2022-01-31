1f343e1c7e2600cdd1d19ab9f516d8e17f23ff24
Cody Olsen <stipsan@gmail.com>
2022-01-31 02:57:05 +0100

    fix: woops ok

--- test/functional/commands.js
+++ test/functional/commands.js
@@ -4,15 +4,15 @@ import Redis from 'ioredis'
 describe('commands', () => {
   test('Calling unsupported command throws a descriptive error', () => {
     const redis = new Redis()
-    expect(() => redis.unsupported()).toThrowErrorMatchingInlineSnapshot(
-      `"Unsupported command: \\"unsupported\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
+    expect(() => redis.debug()).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported command: \\"debug\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
     )
   })
 
   test('Calling unsupported buffer command throws a descriptive error', () => {
     const redis = new Redis()
-    expect(() => redis.unsupportedBuffer()).toThrowErrorMatchingInlineSnapshot(
-      `"Unsupported command: \\"unsupportedBuffer\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
+    expect(() => redis.debugBuffer()).toThrowErrorMatchingInlineSnapshot(
+      `"Unsupported command: \\"debugBuffer\\", please check the full list over mocked commands: https://github.com/stipsan/ioredis-mock/blob/main/compat.md#supported-commands-"`
     )
   })
 })
