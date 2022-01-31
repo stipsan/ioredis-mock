dddff037f7f714c9fce3dccdeaf6f9093d0ec06e
Cody Olsen <stipsan@gmail.com>
2022-01-31 11:04:34 +0100

    refactor: remove jest.js
    BREAKING CHANGE: update `require('ioredis-mock/jest')` calls to `require('ioredis-mock')`

--- jest.config.jestlegacy.js
+++
@@ -1,8 +0,0 @@
-// Runs the integration test suite on the deprecated legacy jest.js entrypoint
-
-module.exports = {
-  // eslint-disable-next-line global-require
-  ...require('./jest.config'),
-  testMatch: ['**/test/integration/**/*.js'],
-  setupFiles: ['<rootDir>/testSetupJestLegacy.js'],
-}
