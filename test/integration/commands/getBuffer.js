8bf80ff78171c46420794528acbf185d53c6cfaf
Ryan Witt <onecreativenerd@gmail.com>
2022-02-27 12:57:22 -0500

    cleanup tests

--- test/integration/commands/getBuffer.js
+++ test/integration/commands/getBuffer.js
@@ -1,9 +1,7 @@
 import Redis from 'ioredis'
 
-// eslint-disable-next-line import/no-relative-parent-imports
-
 describe('getBuffer', () => {
-  it('should return value of a binary key unchanged', async () => {
+  it('should return value of a binary set unchanged', async () => {
     const redis = new Redis()
     const value = Buffer.from('1T+HJWDNyTa4jJXwoBbV6Q==', 'base64')
     await redis.set('foo', value)
