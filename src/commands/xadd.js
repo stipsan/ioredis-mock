bcf569899f08f1627f47ebc9742efe25d266cbf8
Christopher Eck <13987499+chris-codaio@users.noreply.github.com>
2022-03-22 11:28:39 -0700

    feat: Add MAXLEN support in xadd command (#1159)
    * Add MAXLEN support in xadd command
    
    * Fix linter warning
    
    * Appease prettier

--- src/commands/xadd.js
+++ src/commands/xadd.js
@@ -3,14 +3,29 @@ export function xadd(stream, id, ...args) {
     !stream ||
     !id ||
     args.length === 0 ||
-    (args.includes('~') ? args.length < 4 : args.length % 2 !== 0)
+    (args.includes('~') || args.includes('=')
+      ? args.length < 4
+      : args.length % 2 !== 0)
   ) {
     throw new Error("ERR wrong number of arguments for 'xadd' command")
   }
   if (!this.data.has(stream)) {
     this.data.set(stream, [])
   }
-  const eventId = `${id === '*' ? this.data.get(stream).length + 1 : id}-0`
+
+  let threshold
+  let keyId = id
+  if (keyId === 'MAXLEN') {
+    threshold = args.shift()
+    if (threshold === '=' || threshold === '~') {
+      threshold = args.shift()
+    }
+    keyId = args.shift()
+  }
+
+  const eventId = `${
+    keyId === '*' ? this.data.get(stream).length + 1 : keyId
+  }-0`
   const list = this.data.get(stream)
 
   if (list.length > 0 && list[0][0] === `${eventId}`) {
@@ -20,7 +35,11 @@ export function xadd(stream, id, ...args) {
   }
 
   this.data.set(`stream:${stream}:${eventId}`, { polled: false })
-  this.data.set(stream, list.concat([[`${eventId}`, [...args]]]))
 
+  let newData = list.concat([[`${eventId}`, [...args]]])
+  if (threshold && newData.length > threshold) {
+    newData = newData.slice(-threshold)
+  }
+  this.data.set(stream, newData)
   return `${eventId}`
 }
