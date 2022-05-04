ac35d5027758beaae12cda184b8ba288342395ce
Cody Olsen <stipsan@gmail.com>
2022-05-05 01:07:52 +0200

    fix(del): throws if wrong number of args
    closes #1168

--- src/commands/del.js
+++ src/commands/del.js
@@ -1,6 +1,10 @@
 import { emitNotification } from '../keyspace-notifications'
 
 export function del(...keys) {
+  if (keys.length === 0 || !keys[0]) {
+    throw new Error("ERR wrong number of arguments for 'del' command")
+  }
+
   let deleted = 0
   keys.forEach(key => {
     if (this.data.has(key)) {
--- test/integration/commands/del.js
+++ test/integration/commands/del.js
@@ -5,21 +5,34 @@ import { runTwinSuite } from '../../../test-utils'
 
 runTwinSuite('del', command => {
   describe(command, () => {
+    const redis = new Redis()
+
+    afterAll(() => redis.disconnect())
+
     it('should delete passed in keys', async () => {
-      const redis = new Redis()
       await redis.set('deleteme', 'please')
       await redis.set('metoo', 'pretty please')
 
       expect(await redis[command]('deleteme', 'metoo')).toBe(2)
       expect(await redis.get('deleteme')).toBe(null)
       expect(await redis.get('metoo')).toBe(null)
-      redis.disconnect()
     })
     it('return 0 if nothing is deleted', async () => {
-      const redis = new Redis()
-
       expect(await redis[command]('deleteme', 'metoo')).toBe(0)
-      redis.disconnect()
+    })
+
+    it('return 0 if nothing is deleted', async () => {
+      expect(await redis[command]('deleteme', 'metoo')).toBe(0)
+    })
+
+    it('throws if too few arguments', async () => {
+      expect.assertions(1)
+
+      try {
+        await redis[command]()
+      } catch (err) {
+        expect(err.message).toMatchSnapshot()
+      }
     })
   })
 })
