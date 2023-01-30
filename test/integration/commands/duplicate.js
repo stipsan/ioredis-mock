af9acfd2802ef65253b6a98fa2602fd4ecda98d2
Nikita <dev.nkh@gmail.com>
2023-01-30 15:00:26 +0100

    let -> const

--- test/integration/commands/duplicate.js
+++ test/integration/commands/duplicate.js
@@ -5,11 +5,11 @@ describe('duplicate', () => {
     expect(new Redis().options.lazyConnect).toBe(false)
   })
   it('should preserve options', () => {
-    let redis = new Redis({ lazyConnect: true })
+    const redis = new Redis({ lazyConnect: true })
     expect(redis.duplicate().options.lazyConnect).toBe(true)
   })
   it('should handle overrides', () => {
-    let redis = new Redis({ lazyConnect: true })
+    const redis = new Redis({ lazyConnect: true })
     expect(redis.duplicate({ lazyConnect: false }).options.lazyConnect).toBe(false)
   })
 })
