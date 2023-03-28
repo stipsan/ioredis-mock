73ca47e3a54bfe30ca5aa3c56ec1749195b1a8f8
Cody Olsen <stipsan@gmail.com>
2023-03-29 00:31:44 +0200

    chore(e2e): add `pexpiretime` test

---
+++ test/integration/commands/pexpiretime.js
@@ -0,0 +1,31 @@
+import Redis from 'ioredis'
+
+// eslint-disable-next-line import/no-relative-parent-imports
+import { runTwinSuite } from '../../../test-utils'
+
+runTwinSuite('pexpiretime', command => {
+  describe(command, () => {
+    const redis = new Redis()
+
+    afterAll(() => {
+      redis.disconnect()
+    })
+
+    it('should return Unix timestamp of which the key expires', async () => {
+      const at = Date.now() + 100
+      await redis.set('foo', 'bar')
+      await redis.pexpireat('foo', at)
+
+      expect(await redis[command]('foo')).toBe(at)
+    })
+
+    it('should return -1 if key exists but no expiration time attached', async () => {
+      await redis.set('foo', 'bar')
+      expect(await redis[command]('foo')).toBe(-1)
+    })
+
+    it('should return -2 if key does not exist', async () => {
+      expect(await redis[command]('foo')).toBe(-2)
+    })
+  })
+})
