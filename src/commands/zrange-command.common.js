44f89d1b959796024ca1d11a3312ac92200bfec8
Marco Reni <reni.marco@gmail.com>
2023-04-07 20:27:07 +0200

    feat: new zrange implementation to handle all new parameters

--- src/commands/zrange-command.common.js
+++ src/commands/zrange-command.common.js
@@ -1,3 +1,5 @@
+import orderBy from 'lodash.orderby';
+
 export function slice(arr, start, end) {
   return arr.slice(start, end === -1 ? undefined : end + 1)
 }
@@ -74,4 +76,138 @@ export function getWithScoresAndLimit(args) {
   }
 
   return { withScores, limit, offset }
+}
+
+
+function streq(a, b) {
+  return a.toString().toLowerCase() === b.toString().toLowerCase()
+}
+
+export const DIRECTION_REVERSE = 'reverse';
+const DIRECTION_FORWARD = 'forward';
+
+export const RANGE_RANK = 'rank';
+export const RANGE_LEX = 'lex';
+export const RANGE_SCORE = 'score';
+
+/** https://github.com/redis/redis/blob/f651708a19b4fc8137eec13180fcea39e68fb284/src/t_zset.c#L3589 */
+export function zrangeBaseCommand(args, argsStart = 0, store = false, inputRange = null, inputDirection = null) {
+  const key = args[argsStart];
+  const map = this.data.get(key)
+  if (!map) {
+    return []
+  }
+
+  // @TODO investigate a more stable way to detect sorted lists
+  if (this.data.has(key) && !(this.data.get(key) instanceof Map)) {
+    return []
+  }
+
+  let withScores = false;
+  let offset = 0;
+  let limit = null;
+  let direction = inputDirection || DIRECTION_FORWARD;
+  let range = inputRange || RANGE_RANK;
+  let start;
+  let end;
+
+  const minIdx = argsStart + 1;
+  const maxIdx = argsStart + 2;
+
+
+  /* Step 1: Skip the <src> <min> <max> args and parse remaining optional arguments. */
+  for (let j = argsStart + 3; j < args.length; j++) {
+    const leftargs = args.length - j - 1;
+    
+    if (!store && streq(args[j], 'withscores')) {
+      withScores = 1;
+    } else if (streq(args[j], 'limit') && leftargs >= 2) {
+      offset = parseInt(args[j + 1], 10);
+      limit = parseInt(args[j + 2], 10);
+      if (Number.isNaN(offset) || Number.isNaN(limit)) {
+        throw new Error('ERR syntax error')
+      }
+
+      j += 2;
+    } else if (!inputDirection && streq(args[j], 'rev')) {
+      direction = DIRECTION_REVERSE;
+    } else if (!inputRange && streq(args[j] , 'bylex')) {
+      range = RANGE_LEX;
+    } else if (!inputRange && streq(args[j], 'byscore')) {
+      range = RANGE_SCORE;
+    } else {
+       throw new Error('ERR syntax error');
+    }
+  }
+
+  if (limit !== null && range === RANGE_RANK) {
+    throw new Error('ERR syntax error, LIMIT is only supported in combination with either BYSCORE or BYLEX');
+  }
+
+  if (withScores && range === RANGE_LEX) {
+    throw new Error('ERR syntax error, WITHSCORES not supported in combination with BYLEX');
+  }
+
+  /* Step 2: Parse the range. */
+  switch (range) {
+    case RANGE_RANK:
+      /* Z[REV]RANGE, ZRANGESTORE [REV]RANGE */
+      start = parseInt(args[minIdx], 10);
+      end = parseInt(args[maxIdx], 10);
+
+      if (Number.isNaN(start) || Number.isNaN(end)) {
+        throw new Error('ERR syntax error ');
+      }
+      break;
+
+    case RANGE_SCORE:
+        /* Z[REV]RANGEBYSCORE, ZRANGESTORE [REV]RANGEBYSCORE */
+        start = parseLimit(args[minIdx]);
+        end = parseLimit(args[maxIdx]);
+        
+        break;
+    // FIXME: handle RANGE_LEX
+    // case ZRANGE_LEX:
+    //     /* Z[REV]RANGEBYLEX, ZRANGESTORE [REV]RANGEBYLEX */
+    //     if (zslParseLexRange(c->argv[minidx], c->argv[maxidx], &lexrange) != C_OK) {
+    //         addReplyError(c, "min or max not valid string range item");
+    //         return;
+    //     }
+    //     break;
+    // }
+    default: 
+      throw new Error('ERR syntax error');
+  }
+  
+  let sort;
+  if (direction === DIRECTION_REVERSE) {
+    sort = ['desc', 'desc']
+  }
+
+  let ordered;
+  if (range === RANGE_SCORE) {
+    const filteredArray = Array.from(map.values()).filter(
+      filterPredicate(start, end)
+    )
+
+    ordered = orderBy(filteredArray, ['score', 'value'], sort);
+  } 
+  // FIXME: handle RANGE_LEX
+  else {
+    ordered = slice(
+      orderBy(Array.from(map.values()), ['score', 'value'], sort),
+      start,
+      end
+    )
+  }
+
+  if (limit !== null) {
+    ordered = offsetAndLimit(ordered, offset, limit)
+  }
+  
+  if (withScores) {
+    return ordered.flatMap(it => [it.value, `${it.score}`])
+  }
+
+  return ordered.map(it => it.value);
 }
--- src/commands/zrange.js
+++ src/commands/zrange.js
@@ -1,36 +1,8 @@
-import orderBy from 'lodash.orderby'
-
 import { convertStringToBuffer } from '../commands-utils/convertStringToBuffer'
-import { slice } from './zrange-command.common'
+import { zrangeBaseCommand } from './zrange-command.common';
 
-export function zrange(key, s, e, withScores) {
-  const map = this.data.get(key)
-  if (!map) {
-    return []
-  }
-
-  // @TODO investigate a more stable way to detect sorted lists
-  if (this.data.has(key) && !(this.data.get(key) instanceof Map)) {
-    return []
-  }
-
-  const start = parseInt(s, 10)
-  const end = parseInt(e, 10)
-
-  const ordered = slice(
-    orderBy(Array.from(map.values()), ['score', 'value']),
-    start,
-    end
-  )
-
-  if (
-    typeof withScores === 'string' &&
-    withScores.toUpperCase() === 'WITHSCORES'
-  ) {
-    return ordered.flatMap(it => [it.value, `${it.score}`])
-  }
-
-  return ordered.map(it => it.value)
+export function zrange(...args) {
+  return zrangeBaseCommand.call(this, args);
 }
 
 export function zrangeBuffer(...args) {
