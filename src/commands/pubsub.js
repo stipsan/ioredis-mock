fd5ac3c73ae38d2d174fc659a8f5d1e76754e1a8
Paul Hawxby <1090602+phawxby@users.noreply.github.com>
2023-04-13 16:12:37 +0100

    feat: add basic pubsub

---
+++ src/commands/pubsub.js
@@ -0,0 +1,26 @@
+import patternMatchesString from '../commands-utils/patternMatchesString'
+
+export function pubsub(subCommand, pattern) {
+  switch (subCommand) {
+    case 'CHANNELS': {
+      let channels = [];
+
+      this.channels?.instanceListeners?.forEach(
+        (instanceMap, channel) => {
+          channels.push(channel);
+        }
+      )
+
+      if (pattern) {
+        channels = channels.filter((x) => patternMatchesString(pattern, x));
+      }
+
+      return channels;
+    }
+    default: {
+      throw new Error('Currently not implemented as a mock')
+    }
+  }
+}
+
+export const pubsubBuffer = pubsub
---
+++ test/integration/commands/pubsub.js
@@ -0,0 +1,62 @@
+import Redis from 'ioredis'
+
+// eslint-disable-next-line import/no-relative-parent-imports
+import { runTwinSuite } from '../../../test-utils'
+
+const testChannels = [
+  'emails',
+  'messages',
+  'feed',
+];
+
+runTwinSuite('pubsub', command => {
+  // @TODO Rewrite test suite so it runs on a real Redis instance
+  ;(process.env.IS_E2E ? describe.skip : describe)(command, () => {
+    describe('CHANNELS', () => {
+      let redis;
+
+      beforeEach(() => {
+        redis = new Redis({
+          host: 'pubsub',
+        });
+      })
+
+      afterEach(async() => {
+        await Promise.all(testChannels.map((x) => redis.unsubscribe(x)));
+      })
+
+      test('should return 0 when publishing without subscribers', async () => {
+        expect(await redis[command]('CHANNELS')).toMatchObject([])
+      })
+
+      it('should return the single active channel', async () => {
+        redis.subscribe('emails')
+
+        expect(await redis[command]('CHANNELS')).toMatchObject(['emails'])
+      })
+
+      it('should return multiple active channels', async () => {
+        redis.subscribe('emails')
+        redis.subscribe('messages')
+        redis.subscribe('feed')
+
+        expect(await redis[command]('CHANNELS')).toMatchObject([
+          'emails',
+          'messages',
+          'feed',
+        ])
+      })
+
+      it('should return filtered channels', async () => {
+        redis.subscribe('emails')
+        redis.subscribe('messages')
+        redis.subscribe('feed')
+
+        expect(await redis[command]('CHANNELS', 'email*')).toMatchObject([
+          'emails',
+
+        ])
+      })
+    })
+  })
+})
