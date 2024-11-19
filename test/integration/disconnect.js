e9a22f29004cbfef5e6a9939688696652f2d0d9e
Степан Логвин <stepan.logvin@x5.ru>
2024-11-19 16:50:14 +0300

    test: Modify test

--- test/integration/disconnect.js
+++ test/integration/disconnect.js
@@ -1,8 +1,16 @@
 import Redis from 'ioredis'
 
 describe('disconnect', () => {
-  it('should be available, but do nothing', () => {
+  it('should be available and emit "end" and "close" events', async () => {
     const redis = new Redis({})
+    const events = []
+    redis.on('close', () => events.push('close'))
+    redis.on('end', () => events.push('end'))
+
     expect(redis.disconnect()).toBe(undefined)
+
+    await new Promise(resolve => setImmediate(resolve));
+
+    expect(events).toEqual(['close', 'end'])
   })
 })
