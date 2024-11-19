18c16dae298d6b9cfe42dbece500420d5dbf097f
Степан Логвин <stepan.logvin@x5.ru>
2024-11-19 17:19:58 +0300

    revert: Remove close event

--- src/commands-utils/emitDisconnectEvent.js
+++ src/commands-utils/emitDisconnectEvent.js
@@ -1,6 +1,5 @@
 export default function emitConnectEvent(redisMock) {
   process.nextTick(() => {
-    redisMock.emit('close')
     redisMock.emit('end')
   })
 }
--- test/integration/disconnect.js
+++ test/integration/disconnect.js
@@ -1,16 +1,15 @@
 import Redis from 'ioredis'
 
 describe('disconnect', () => {
-  it('should be available and emit "end" and "close" events', async () => {
+  it('should be available and emit "end" event', async () => {
     const redis = new Redis({})
     const events = []
-    redis.on('close', () => events.push('close'))
     redis.on('end', () => events.push('end'))
 
     expect(redis.disconnect()).toBe(undefined)
 
     await new Promise(resolve => setImmediate(resolve));
 
-    expect(events).toEqual(['close', 'end'])
+    expect(events).toEqual(['end'])
   })
 })
